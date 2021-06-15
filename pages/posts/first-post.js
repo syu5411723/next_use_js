c7cc423fa3e436299cdb1ad9c38ee86c08af91ae
maekawa-syuki <syu54117123>
2021-06-15 14:26:05 +0900

    commti

---
+++ pages/posts/first-post.js
@@ -0,0 +1,30 @@
+﻿import React from 'react'
+import styled from 'styled-components'
+import Head from 'next/head'
+import Link from 'next/link'
+
+import Layout from '../../components/layout'
+
+const A = styled.a`
+    color: yellow;
+`
+
+const fisrtPost = () => {
+    return (
+        <>
+            <Layout>
+                <Head>
+                    <title>Frirst Post</title>
+                </Head>
+                <h1>First Post</h1>
+                <h2>
+                    <Link href="/" >
+                        <a>Back to home</a>
+                    </Link>
+                </h2>
+            </Layout>
+        </>
+    )
+}
+
+export default fisrtPost
---
+++ pages/second-post.js
@@ -0,0 +1,24 @@
+﻿import React from 'react'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    width:80vw;
+    height: 80vh;
+    margin: 0 auto;
+    display:flex;
+    justify-content:center;
+    align-items:center;
+`
+const H1 = styled.h1``
+
+const secondPost = () => {
+    return (
+        <>
+            <Wrapper>
+                <H1>Second Post</H1>
+            </Wrapper>
+        </>
+    )
+}
+
+export default secondPost
